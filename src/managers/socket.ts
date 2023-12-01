eb1992a09737c6ebe594ddb0b2dbdc8ab282a48c
SnaffX <gameradriel737@gmail.com>
2023-12-01 17:01:45 -0300

    feat: remove code in socket close event

--- src/managers/socket.ts
+++ src/managers/socket.ts
@@ -5,45 +5,45 @@ import { GiggleAPI } from "..";
 import { PaymentEvent } from "../types/events";
 
 type Events = {
-   error: (error: Error) => void;
-   connect: () => void;
-   payment: (data: PaymentEvent) => void;
+  error: (error: Error) => void;
+  connect: () => void;
+  payment: (data: PaymentEvent) => void;
 };
 
 export class SocketManager extends (EventEmitter as new () => TypedEmitter<Events>) {
-   private ws: WebSocket;
+  private ws: WebSocket;
 
-   constructor(private client: GiggleAPI) {
-      super();
-   }
+  constructor(private client: GiggleAPI) {
+    super();
+  }
 
-   connect() {
-      const url =
-         this.client.props?.base_url ??
-         "wss://giggle-api.squareweb.app/ws/connect";
-      try {
-         this.ws = new WebSocket(url, {
-            headers: {
-               authorization: this.client.api_key,
-            },
-         });
+  connect() {
+    const url =
+      this.client.props?.base_url ??
+      "wss://giggle-api.squareweb.app/ws/connect";
+    try {
+      this.ws = new WebSocket(url, {
+        headers: {
+          authorization: this.client.api_key,
+        },
+      });
 
-         this.ws.on("error", err => this.emit("error", err));
-         this.ws.on("open", () => this.emit("connect"));
-         this.ws.on("message", message => {
-            const parsed = JSON.parse(message.toString());
+      this.ws.on("error", (err) => this.emit("error", err));
+      this.ws.on("open", () => this.emit("connect"));
+      this.ws.on("message", (message) => {
+        const parsed = JSON.parse(message.toString());
 
-            switch (parsed.event) {
-               case "payment":
-                  this.emit("payment", parsed.data);
-            }
-         });
-         this.ws.on("close", (code: number) => {
-            setTimeout(
-               () => this.connect(),
-               this.client.props?.reconnect_timeout ?? 1000 * 10
-            );
-         });
-      } catch (err) {}
-   }
+        switch (parsed.event) {
+          case "payment":
+            this.emit("payment", parsed.data);
+        }
+      });
+      this.ws.on("close", () => {
+        setTimeout(
+          () => this.connect(),
+          this.client.props?.reconnect_timeout ?? 1000 * 10
+        );
+      });
+    } catch (err) {}
+  }
 }
